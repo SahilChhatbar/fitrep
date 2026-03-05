e222ee9af58b4192f73729b55d3c811fe92a7890
Sahil <sahil.chhatbar@elixirtechne.com>
2026-03-05 16:37:34 +0530

    chore: setup mantine up, hooks and prettier config

---
+++ client/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "@mantine/core"
+
+const page = () => {
+    return (
+        <Stack align="center" justify="center" h="100vh">
+            dashboard
+        </Stack>
+    )
+}
+
+export default page
