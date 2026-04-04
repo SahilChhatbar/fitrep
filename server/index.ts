1043c3b6a22cc1ed4d46e330730a0477fb91bf96
Sahil Chhatbar <sahilchhatbar7@gmail.com>
2026-04-05 00:45:02 +0530

    chore: try to fix deployment server issues

--- server/index.ts
+++ server/index.ts
@@ -10,7 +10,7 @@ import progressRoutes from "./routes/progressRoutes.ts";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.use(express.json());
 app.use(
@@ -58,6 +58,6 @@ app.use((_req, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+const server = app.listen(PORT, "0.0.0.0", () => {
+  console.log("server running on port", PORT);
 });
